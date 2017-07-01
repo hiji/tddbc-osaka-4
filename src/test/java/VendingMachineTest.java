e0cb6d3083d21759ad6a30796cc3a7dd356a440f
Shinya Hijiri <shinya.hijiri@gmail.com>
2017-07-01 09:19:07 +0900

    初期構築

---
+++ src/test/java/VendingMachineTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by hijiri on 2017/07/01.
+ */
+public class VendingMachineTest {
+
+    @Test
+    public void こける() throws Exception {
+        assertThat(true, is(false));
+    }
+}
